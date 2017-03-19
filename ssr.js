4c79dcd1779070381849db7f2a2b527f47b8382b
defcc <chengchao1@gmail.com>
2017-03-20 01:30:31 +0800

    init

--- ssr.js
+++ ssr.js
@@ -1,21 +1,3 @@
-# v-show-ssr-bug
-
-This repo is used to reproduces the SSR v-show render bug.
-
-## env
-vue@latest
-
-## steps
-
-1. `git clone https://github.com/defcc/v-show-ssr-bug.git v-show-ssr-bug`
-2. `npm install`
-3. `npm test`
-
-The result is `<div server-rendered="true">inner</div>`, it should be `<div server-rendered="true" style="display:none;">inner</div>`
-
-## demo code
-
-```js
 var Vue = require('vue/dist/vue.common.js')
 var createRenderer = require('vue-server-renderer').createRenderer
 var renderToString = createRenderer().renderToString
@@ -44,7 +26,3 @@ const vm = new Vue({
 renderToString(vm, (err, res) => {
   console.log(res)
 })
-
-```
-
-The 
